defb0dac6ecebe42059dab699d04db115c51f955
Vitor Henrique <vitorhenriquecardoso59@gmail.com>
2025-07-09 20:44:04 -0300

    new

---
+++ AstronomyPictureOfTheDay/assets/nasa-api.js
@@ -0,0 +1,41 @@
+const nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key=fKv1OFFYQQ8NRXdEIPUH6cePWOCXmAL6UJEwo6Ny'
+
+fetch(nasaUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+async function carregarDados() {
+    try {
+        const resposta = await fetch(nasaUrl);
+        // 1) Transforma o corpo da resposta em JSON
+        const dados = await resposta.json();
+
+        console.log(dados);
+
+        // 3) Extraia as propriedades de que precisa…
+        // (ajusta os nomes conforme o que a API devolve)
+        const { date, explanation, hdurl, title, } = dados;
+
+        // 4) …e jogue no DOM (ou em estado, etc.)
+        document.querySelector('#date').textContent = date;
+        document.querySelector('#explanation').textContent = explanation;
+        document.querySelector('#title').textContent = title;
+        document.querySelector('#image').src = hdurl;
+        document.querySelector('#image').alt = title;
+
+    } catch (erro) {
+        console.error('Deu ruim na chamada da API', erro);
+    }
+}
+
+carregarDados();
